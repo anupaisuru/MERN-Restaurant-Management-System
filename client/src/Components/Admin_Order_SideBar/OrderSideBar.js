8e5202a6b9eac0dbd8c895939e881006c740afb0
Anupa Isuru <anupaisuru2002@gmail.com>
2023-12-10 12:05:42 +0530

    change side menu

--- client/src/Components/Admin_Order_SideBar/OrderSideBar.js
+++ client/src/Components/Admin_Order_SideBar/OrderSideBar.js
@@ -10,84 +10,90 @@ import CancelSharpIcon from "@mui/icons-material/CancelSharp";
 import { useState } from "react";
 
 const items = [
-    {
-        id: 1,
-        name: "DashBoard",
-        icon: <HomeSharpIcon />,
-        path: "/order/dashboard",
-    },
+  {
+    id: 8,
+    name: "Admin DashBoard",
+    icon: <HomeSharpIcon />,
+    path: "/dashboard",
+  },
+  {
+    id: 1,
+    name: "Order DashBoard",
+    icon: <HomeSharpIcon />,
+    path: "/order/dashboard",
+  },
 
-    {
-        id: 2,
-        name: "Orders",
-        icon: <ContentPasteIcon />,
-        path: "/order/orders",
-    },
+  {
+    id: 2,
+    name: "Orders",
+    icon: <ContentPasteIcon />,
+    path: "/order/orders",
+  },
 
-    {
-        id: 3,
-        name: "Pending Orders",
-        icon: <PendingActionsSharpIcon />,
-        path: "/order/pending",
-    },
+  {
+    id: 3,
+    name: "Pending Orders",
+    icon: <PendingActionsSharpIcon />,
+    path: "/order/pending",
+  },
 
-    {
-        id: 4,
-        name: "Completed Orders",
-        icon: <LibraryAddCheckSharpIcon />,
-        path: "/order/complete",
-    },
+  {
+    id: 4,
+    name: "Completed Orders",
+    icon: <LibraryAddCheckSharpIcon />,
+    path: "/order/complete",
+  },
 
-    {
-        id: 5,
-        name: "Delivering Orders",
-        icon: <DeliveryDiningSharpIcon />,
-        path: "/order/delivering",
-    },
+  {
+    id: 5,
+    name: "Delivering Orders",
+    icon: <DeliveryDiningSharpIcon />,
+    path: "/order/delivering",
+  },
 
-    {
-        id: 6,
-        name: "Delivered Orders",
-        icon: <CheckBoxSharpIcon />,
-        path: "/order/deliver",
-    },
+  {
+    id: 6,
+    name: "Delivered Orders",
+    icon: <CheckBoxSharpIcon />,
+    path: "/order/deliver",
+  },
 
-    {
-        id: 7,
-        name: "Cancelled Orders",
-        icon: <CancelSharpIcon />,
-        path: "/order/cancel",
-    },
+  {
+    id: 7,
+    name: "Cancelled Orders",
+    icon: <CancelSharpIcon />,
+    path: "/order/cancel",
+  },
 ];
 
 function OrderSideBar() {
-    const [active, setActive] = useState(1);
+  const [active, setActive] = useState(1);
 
-    const handleActive = (id) => {
-        setActive(id);
-        console.log(id)
-    }
+  const handleActive = (id) => {
+    setActive(id);
+    console.log(id);
+  };
 
-    return (
-        <div className="sidebar-container">
-            {items.map((item) => (
-                <Link
-                    to={item.path}
-                    key={item.id}
-                    className="sidebar-item"
-                    onClick={() => handleActive(item.id)}
-                    style={{
-                        background: active === item.id ? "#CBCAFF" : "",
-                    }}
-                >
-                    <div className="icon-name">
-                        {item.icon}
-                        {item.name}
-                    </div>
-                </Link>
-            ))}
-        </div>
-    );
+  return (
+    <div className="sidebar-container">
+      {items.map((item) => (
+        <Link
+          to={item.path}
+          key={item.id}
+          className="sidebar-item"
+          onClick={() => handleActive(item.id)}
+          style={{
+            background: active === item.id ? "#CBCAFF" : "",
+          }}
+        >
+          <div className="icon-name">
+            {item.icon}
+            {item.name}
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default OrderSideBar;
--- client/src/Components/sideMenu/Sidemenu.js
+++ client/src/Components/sideMenu/Sidemenu.js
@@ -1,70 +1,52 @@
-import React from 'react';
-import "./Sidemenu.css"
-import HomeIcon from '@mui/icons-material/Home';
-import SupportAgentIcon from '@mui/icons-material/SupportAgent';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
-import ContentPasteIcon from '@mui/icons-material/ContentPaste';
-import GroupsIcon from '@mui/icons-material/Groups';
-import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
-import Inventory2Icon from '@mui/icons-material/Inventory2';
+import React from "react";
+import "./Sidemenu.css";
+import HomeIcon from "@mui/icons-material/Home";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import ContentPasteIcon from "@mui/icons-material/ContentPaste";
+import GroupsIcon from "@mui/icons-material/Groups";
 
 export const SidemenuData = [
   {
     title: "Dashboard",
     icon: <HomeIcon />,
-    link: "/dashboard"
+    link: "/dashboard",
   },
 
   {
     title: "Customers",
     icon: <AccountBoxIcon />,
-    link: "/customer"
+    link: "/customer",
   },
   {
     title: "Products",
     icon: <ShoppingCartIcon />,
-    link: "/products"
+    link: "/products",
   },
-  {
-    title: "Transaction",
-    icon: <LibraryBooksIcon />,
-    link: "/transaction"
-  },
+
   {
     title: "Order",
     icon: <ContentPasteIcon />,
-    link: "/order/dashboard"
+    link: "/order/dashboard",
   },
 
   {
     title: "Employees",
     icon: <GroupsIcon />,
-    link: "/employees"
-  },
-  {
-    title: "Suppliers",
-    icon: <SupervisedUserCircleIcon />,
-    link: "/suppliers"
-  },
-  {
-    title: "Stock",
-    icon: <Inventory2Icon />,
-    link: "/stock"
+    link: "/employees",
   },
 
   {
     title: "Managers",
     icon: <SupportAgentIcon />,
-    link: "/subadmin"
+    link: "/subadmin",
   },
-
 ];
 
 function Sidemenu() {
   return (
-    <div className="SideMenu col-2" >
+    <div className="SideMenu col-2">
       <ul className="sidebarList">
         {SidemenuData.map((val, key) => {
           return (
@@ -83,7 +65,6 @@ function Sidemenu() {
         })}
       </ul>
     </div>
-
   );
 }
 
